e19d3a51366cb72dce75e7533d7e11747b56297a
alanltr <letrocqueralan@gmail.com>
2021-05-05 17:12:46 +0200

    style pour la modal du mail

--- src/components/Content/MailModal/index.js
+++ src/components/Content/MailModal/index.js
@@ -3,51 +3,58 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
+import TextField from '@material-ui/core/TextField';
 
 // == Import
 import './mailModal.scss';
 
-// function rand() {
-//   return Math.round(Math.random() * 20) - 10;
-// }
-
-// function getModalStyle() {
-//   const top = 50 + rand();
-//   const left = 50 + rand();
-
-//   return {
-//     top: `${top}%`,
-//     left: `${left}%`,
-//     transform: `translate(-${top}%, -${left}%)`,
-//   };
-// }
-
-// const useStyles = makeStyles((theme) => ({
-//   paper: {
-//     position: 'absolute',
-//     width: 400,
-//     backgroundColor: theme.palette.background.paper,
-//     border: '2px solid #000',
-//     boxShadow: theme.shadows[5],
-//     padding: theme.spacing(2, 4, 3),
-//   },
-// }));
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
 
 // == Composant
 const MailModal = ({
   isOpen,
   toggleIsOpen,
 }) => {
-  // const classes = useStyles();
-  // // getModalStyle is not a pure function, we roll the style only on the first render
-  // const [modalStyle] = React.useState(getModalStyle);
+  const classes = useStyles();
 
   const body = (
     <div className="modal-component">
-      <h2 id="simple-modal-title">Text in a modal</h2>
-      <p id="simple-modal-description">
-        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-      </p>
+      <h2 id="simple-modal-title">Formulaire de contact</h2>
+      <div id="simple-modal-description">
+        <form className={classes.root} noValidate autoComplete="off">
+          <TextField
+            type="email"
+            id="filled-secondary"
+            label="Votre email"
+            variant="filled"
+            fullWidth
+          />
+          <TextField
+            type="text"
+            id="filled-secondary"
+            label="L'objet de votre mail (xx caractères max)"
+            variant="filled"
+            fullWidth
+          />
+          <TextField
+            id="filled-multiline-static"
+            label="Votre mail ici (xx caractères max)"
+            multiline
+            rows={5}
+            variant="filled"
+          />
+          <div className="submit-btn-div glassmorphism">
+            <button aria-label="submit-btn" type="submit">Send</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 
