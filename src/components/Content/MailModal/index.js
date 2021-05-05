79b4fca1bc70b5fe4793ce7e80b3a7863a357fd0
alanltr <letrocqueralan@gmail.com>
2021-05-05 20:25:29 +0200

    ajout de style pour la modal mail

--- src/components/Content/MailModal/index.js
+++ src/components/Content/MailModal/index.js
@@ -1,51 +1,37 @@
 // == Import npm
 import React from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import TextField from '@material-ui/core/TextField';
 
 // == Import
 import './mailModal.scss';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
-      width: '25ch',
-    },
-  },
-}));
-
 // == Composant
 const MailModal = ({
   isOpen,
   toggleIsOpen,
 }) => {
-  const classes = useStyles();
-
   const body = (
     <div className="modal-component glassmorphism">
       <h2 id="simple-modal-title">Formulaire de contact</h2>
       <div id="simple-modal-description">
-        <form className={classes.root} noValidate autoComplete="off">
+        <form noValidate autoComplete="off">
           <TextField
             type="email"
             id="filled-secondary"
             label="Votre email"
             variant="filled"
-            fullWidth
           />
           <TextField
             type="text"
             id="filled-secondary"
-            label="L'objet de votre mail (xx caractères max)"
+            label="L'objet de votre mail (70 caractères max)"
             variant="filled"
-            fullWidth
           />
           <TextField
             id="filled-multiline-static"
-            label="Votre mail ici (xx caractères max)"
+            label="Votre mail ici (300 caractères max)"
             multiline
             rows={5}
             variant="filled"
